73616e3affa392ea6b269de7c9a745a43609c656
darshanpding <darshan@pd-ing.com>
2024-06-18 18:41:26 +0530

    Added fcm feature

---
+++ src/main/java/com/pding/paymentservice/controllers/DeviceTokenController.java
@@ -0,0 +1,75 @@
+package com.pding.paymentservice.controllers;
+
+import com.pding.paymentservice.payload.request.fcm.DeviceTokenRequest;
+import com.pding.paymentservice.payload.request.fcm.SendNotificationRequest;
+import com.pding.paymentservice.payload.response.ErrorResponse;
+import com.pding.paymentservice.payload.response.generic.GenericStringResponse;
+import com.pding.paymentservice.security.AuthHelper;
+import com.pding.paymentservice.service.DeviceTokenService;
+import com.pding.paymentservice.service.FcmService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/payment/tokens")
+public class DeviceTokenController {
+
+    @Autowired
+    private DeviceTokenService deviceTokenService;
+
+    @Autowired
+    private FcmService fcmService;
+
+    @Autowired
+    private AuthHelper authHelper;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerDeviceToken(@Valid @RequestBody DeviceTokenRequest deviceTokenRequest) {
+        try {
+            String userId = authHelper.getUserId();
+            deviceTokenService.saveOrUpdateDeviceToken(deviceTokenRequest.getToken(), userId);
+            return ResponseEntity.ok().body(new GenericStringResponse(null, "Device Token Registered/Updated Successfully"));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new GenericStringResponse(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()), null));
+        }
+
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteDeviceToken(@Valid @RequestBody DeviceTokenRequest deviceTokenRequest) {
+        try {
+            deviceTokenService.deleteToken(deviceTokenRequest.getToken());
+            return ResponseEntity.ok().body(new GenericStringResponse(null, "Device Token Deleted Successfully"));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new GenericStringResponse(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()), null));
+        }
+
+    }
+
+    @PostMapping("/sendNotification")
+    public ResponseEntity<?> sendNotification(@Valid @RequestBody SendNotificationRequest sendNotificationRequest) {
+        try {
+            String message = fcmService.sendNotification(sendNotificationRequest.getUserId(), sendNotificationRequest.getTitle(), sendNotificationRequest.getBody());
+            return ResponseEntity.ok().body(new GenericStringResponse(null, message));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new GenericStringResponse(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()), null));
+        }
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<?> handleMissingParam(MissingServletRequestParameterException ex) {
+        String paramName = ex.getParameterName();
+        return ResponseEntity.badRequest().body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Required request parameter '" + paramName + "' is missing or invalid."));
+    }
+}
---
+++ src/main/java/com/pding/paymentservice/repository/DeviceTokenRepository.java
@@ -0,0 +1,15 @@
+package com.pding.paymentservice.repository;
+
+import com.pding.paymentservice.models.DeviceToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DeviceTokenRepository extends JpaRepository<DeviceToken, String> {
+    Optional<DeviceToken> findByToken(String token);
+
+    List<DeviceToken> findByUserId(String userId);
+
+    void deleteByToken(String token);
+}
