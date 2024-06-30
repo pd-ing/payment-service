6bf10b935352ddf96624e6acbe71fb861ac8ba83
darshanpding <darshan@pd-ing.com>
2024-06-30 16:17:28 +0530

    Linked the gift send during call to the call node of realtime db

--- src/main/java/com/pding/paymentservice/service/MessagePurchaseService.java
+++ src/main/java/com/pding/paymentservice/service/MessagePurchaseService.java
@@ -5,13 +5,16 @@ import com.pding.paymentservice.models.MessagePurchase;
 import com.pding.paymentservice.models.VideoPurchase;
 import com.pding.paymentservice.models.enums.NotificaitonDataType;
 import com.pding.paymentservice.models.enums.TransactionType;
+import com.pding.paymentservice.repository.CallPurchaseRepository;
 import com.pding.paymentservice.repository.MessagePurchaseRepository;
+import com.pding.paymentservice.util.FirebaseRealtimeDbHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -35,6 +38,12 @@ public class MessagePurchaseService {
     @Autowired
     FcmService fcmService;
 
+    @Autowired
+    CallPurchaseRepository callPurchaseRepository;
+
+    @Autowired
+    FirebaseRealtimeDbHelper firebaseRealtimeDbHelper;
+
     @Transactional
     public String CreateMessageTransaction(String userId,
                                            String pdUserId,
@@ -54,6 +63,7 @@ public class MessagePurchaseService {
         pdLogger.logInfo("MESSAGE_PURCHASE", "Message purchase details recorded in LEDGER MessageId : " + messagedId + ", leafs : " + leafsTransacted + ", TransactionType : " + TransactionType.TEXT_MESSAGE);
 
         if (notifyPd) {
+            addCallTransactionEntryToRealTimeDatabase(messagedId, leafsTransacted);
             try {
                 Map<String, String> data = new HashMap<>();
                 data.put("NotificationType", NotificaitonDataType.GIFT_RECEIVE.getDisplayName());
@@ -67,5 +77,35 @@ public class MessagePurchaseService {
         }
 
         return "Leafs charge was successful";
+    }
+
+
+    //When sending the gift during call it is considered as the earning made during call
+    public void addCallTransactionEntryToRealTimeDatabase(String callId, BigDecimal leafsSpendToSendGift) {
+        try {
+            List<Object[]> callPurchaseList = callPurchaseRepository.findUserIdPdUserIdAndSumLeafsTransactedByCallId(callId);
+            for (Object[] callPurchase : callPurchaseList) {
+                String userId = callPurchase[0].toString();
+                String pdUserID = callPurchase[1].toString();
+                String leafsTransacted = callPurchase[2].toString();
+
+                //Update the info of user and his leafDeduction
+                firebaseRealtimeDbHelper.updateCallChargesDetailsInFirebase(
+                        userId,
+                        callId,
+                        new BigDecimal(leafsTransacted).add(leafsSpendToSendGift),
+                        new BigDecimal(0));
+
+                //Update the info of PD and his leaf Earning
+                firebaseRealtimeDbHelper.updateCallChargesDetailsInFirebase(
+                        pdUserID,
+                        callId,
+                        new BigDecimal(0),
+                        new BigDecimal(leafsTransacted).add(leafsSpendToSendGift));
+            }
+        } catch (Exception e) {
+            pdLogger.logException(e);
+        }
+
     }
 }
