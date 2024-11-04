9bba8de8bdbf6ffc7193bd1f4f694aae3a00cda6
Ben <ben@pd-ing.com>
2024-11-04 14:54:03 +0700

    fix NPE

--- src/main/java/com/pding/paymentservice/models/other/services/tables/dto/ReferralCommissionDetailsDTO.java
+++ src/main/java/com/pding/paymentservice/models/other/services/tables/dto/ReferralCommissionDetailsDTO.java
@@ -1,15 +1,9 @@
 package com.pding.paymentservice.models.other.services.tables.dto;
 
-import com.pding.paymentservice.models.enums.CommissionTransferStatus;
-import com.pding.paymentservice.models.enums.WithdrawalStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -37,23 +31,23 @@ public class ReferralCommissionDetailsDTO {
     public static ReferralCommissionDetailsDTO fromObjectArray(Object[] objectArr) {
         ReferralCommissionDetailsDTO referralCommissionDetailsDTOObj = new ReferralCommissionDetailsDTO();
 
-        referralCommissionDetailsDTOObj.setReferralCommissionId(objectArr[0].toString());
-        referralCommissionDetailsDTOObj.setWithdrawalId(objectArr[1].toString());
-        referralCommissionDetailsDTOObj.setReferrerPdUserId(objectArr[2].toString());
-        referralCommissionDetailsDTOObj.setCommissionPercent(objectArr[3].toString());
-        referralCommissionDetailsDTOObj.setCommissionAmountInTrees(objectArr[4].toString());
-        referralCommissionDetailsDTOObj.setCommissionAmountInCents(objectArr[5].toString());
-        referralCommissionDetailsDTOObj.setReferralCommissionCreatedDate(objectArr[6].toString());
-        referralCommissionDetailsDTOObj.setReferralCommissionUpdatedDate(objectArr[7].toString());
-        referralCommissionDetailsDTOObj.setCommissionTransferStatus(objectArr[8].toString());
-        referralCommissionDetailsDTOObj.setWithdrawalUserId(objectArr[9].toString());
-        referralCommissionDetailsDTOObj.setWithdrawalTrees(objectArr[10].toString());
-        referralCommissionDetailsDTOObj.setWithdrawalLeafs(objectArr[11].toString());
-        referralCommissionDetailsDTOObj.setWithdrawalStatus(objectArr[12].toString());
-        referralCommissionDetailsDTOObj.setWithdrawalCreatedDate(objectArr[13].toString());
-        referralCommissionDetailsDTOObj.setWithdrawalUpdatedDate(objectArr[14].toString());
-        referralCommissionDetailsDTOObj.setUserNickname(objectArr[15].toString());
-        referralCommissionDetailsDTOObj.setPdType(objectArr[16].toString());
+        referralCommissionDetailsDTOObj.setReferralCommissionId(objectArr[0] != null ? objectArr[0].toString() : null);
+        referralCommissionDetailsDTOObj.setWithdrawalId(objectArr[1] != null ? objectArr[1].toString() : null);
+        referralCommissionDetailsDTOObj.setReferrerPdUserId(objectArr[2] != null ? objectArr[2].toString() : null);
+        referralCommissionDetailsDTOObj.setCommissionPercent(objectArr[3] != null ? objectArr[3].toString() : null);
+        referralCommissionDetailsDTOObj.setCommissionAmountInTrees(objectArr[4] != null ? objectArr[4].toString() : null);
+        referralCommissionDetailsDTOObj.setCommissionAmountInCents(objectArr[5] != null ? objectArr[5].toString() : null);
+        referralCommissionDetailsDTOObj.setReferralCommissionCreatedDate(objectArr[6] != null ? objectArr[6].toString() : null);
+        referralCommissionDetailsDTOObj.setReferralCommissionUpdatedDate(objectArr[7] != null ? objectArr[7].toString() : null);
+        referralCommissionDetailsDTOObj.setCommissionTransferStatus(objectArr[8] != null ? objectArr[8].toString() : null);
+        referralCommissionDetailsDTOObj.setWithdrawalUserId(objectArr[9] != null ? objectArr[9].toString() : null);
+        referralCommissionDetailsDTOObj.setWithdrawalTrees(objectArr[10] != null ? objectArr[10].toString() : null);
+        referralCommissionDetailsDTOObj.setWithdrawalLeafs(objectArr[11] != null ? objectArr[11].toString() : null);
+        referralCommissionDetailsDTOObj.setWithdrawalStatus(objectArr[12] != null ? objectArr[12].toString() : null);
+        referralCommissionDetailsDTOObj.setWithdrawalCreatedDate(objectArr[13] != null ? objectArr[13].toString() : null);
+        referralCommissionDetailsDTOObj.setWithdrawalUpdatedDate(objectArr[14] != null ? objectArr[14].toString() : null);
+        referralCommissionDetailsDTOObj.setUserNickname(objectArr[15] != null ? objectArr[15].toString() : null);
+        referralCommissionDetailsDTOObj.setPdType(objectArr[16] != null ? objectArr[16].toString() : null);
 
         return referralCommissionDetailsDTOObj;
     }
