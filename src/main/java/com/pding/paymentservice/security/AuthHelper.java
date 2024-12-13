8fdd8a8628f1c75e5c68c2e915406c773c45deaa
StevePDing <steve@pd-ing.com>
2024-12-13 14:23:31 +0700

    Optimize API Authentication

--- src/main/java/com/pding/paymentservice/security/AuthHelper.java
+++ src/main/java/com/pding/paymentservice/security/AuthHelper.java
@@ -1,40 +1,54 @@
 package com.pding.paymentservice.security;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserRecord;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AuthHelper {
+public class AuthHelper implements FirebaseDataInfo{
 
     public String getUserId() {
-        return getLoggedInFirebaseUser().getUid();
+        Authentication authentication =  SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+        if(principal instanceof PdingSecurityHolder holder){
+            return holder.getUid();
+        }else{
+            return null;
+        }
     }
-
     public String getIdToken() throws Exception {
         return getLoggedInFirebaseUser().idToken;
     }
-
+    @Override
     public String getUserEmail() throws Exception {
         return getLoggedInFirebaseUser().getEmail();
     }
-
+    @Override
     public Boolean isEmailVerified() throws Exception {
         return getLoggedInFirebaseUser().getEmailVerified();
     }
 
-    public LoggedInUserRecord getLoggedInFirebaseUser() {
+    public LoggedInUserRecord getLoggedInFirebaseUser() throws Exception {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication != null && authentication.isAuthenticated()) {
             Object principal = authentication.getPrincipal();
 
-            if (principal instanceof LoggedInUserRecord) {
-                return (LoggedInUserRecord) principal;
+            if (principal instanceof PdingSecurityHolder holder) {
+                UserRecord userRecord = holder.getUserRecord();
+
+                if(userRecord == null) {
+                    userRecord =  FirebaseAuth.getInstance().getUser(holder.getUid());
+                    holder.setUserRecord(userRecord);
+                }
+
+                return LoggedInUserRecord.fromUserRecord(holder.getUserRecord(), holder.getRequest());
             }
         }
 
-        throw new RuntimeException("Login again.");
+        throw new Exception("Login again.");
     }
 
 }
