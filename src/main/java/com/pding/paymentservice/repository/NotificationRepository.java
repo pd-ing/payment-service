a06035980cf007f0db7af4cab5a3c82ca1a4374e
ben <ben@pd-ing.com>
2025-08-03 18:32:59 +0700

    Refactor notification system to use user nickname instead of email

--- src/main/java/com/pding/paymentservice/repository/NotificationRepository.java
+++ src/main/java/com/pding/paymentservice/repository/NotificationRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface NotificationRepository extends JpaRepository<VideoPurchase, String> {
-    @Query(value = "SELECT email FROM users WHERE id = :userId", nativeQuery = true)
-    String findEmailByUserId(@Param("userId") String userId);
+    @Query(value = "SELECT nickname FROM users WHERE id = :userId", nativeQuery = true)
+    String findNicknameByUserId(@Param("userId") String userId);
 
     @Query(value = "SELECT title FROM videos WHERE video_id = :videoId", nativeQuery = true)
     String findTitleByVideoId(@Param("videoId") String videoId);
